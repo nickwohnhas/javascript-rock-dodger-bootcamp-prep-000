87ab43a74937e4b69e417371ba98e76d91104aeb
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-15 16:07:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
     const dodgerRightEdge = dodgerLeftEdge + 40;
     const rockLeftEdge = positionToInteger(rock.style.left)
-    const rockRightEdge = rockLeftEdge + 40;
+    const rockRightEdge = rockLeftEdge + 20;
 
     return (rockLeftEdge<= dodgerLeftEdge && rockRightEdge>=dodgerLeftEdge || 
         rockLeftEdge>=dodgerLeftEdge && rockRightEdge<=dodgerRightEdge || 
