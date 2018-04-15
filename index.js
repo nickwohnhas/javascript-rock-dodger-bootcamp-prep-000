baefef4774327c2f2b52d23520878343f6c8f88a
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-15 15:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,18 +12,13 @@ const START = document.getElementById('start')
 
 var gameInterval = null
 
-/**
- * Be aware of what's above this line,
- * but all of your work should happen below.
- */
+
 
 function checkCollision(rock) {
  
   const top = positionToInteger(rock.style.top)
 
-  // rocks are 20px high
-  // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
+  
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
@@ -32,7 +27,7 @@ function checkCollision(rock) {
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
+    
     const rockRightEdge = rockLeftEdge + 40;
 
     if (rockLeftEdge<= dodgerLeftEdge && rockRightEdge>=dodgerLeftEdge || rockLeftEdge>=dodgerLeftEdge && rockRightEdge<=dodgerRightEdge || rockLeftEdge<=dodgerRightEdge && rockRightEdge>= dodgerRightEdge){ 
@@ -49,13 +44,11 @@ game.appendChild(rock)
   rock.className = 'rock'
   rock.style.left = `${x}px`
 
-  // Hmmm, why would we have used `var` here?
-  var top = 0
+ var top = 0
 
   rock.style.top = top
 
-  
-  function step() { 
+   function step() { 
     rock.style.top = `${top += 2}px`
  
     if (top < 360) {
@@ -67,17 +60,10 @@ game.appendChild(rock)
 
   
   
+
+
+
   
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
-
-
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
   function moveRock() {
    
    if (checkCollision(rock)){
@@ -86,28 +72,10 @@ game.appendChild(rock)
    
    
    
-    // implement me!
-    // (use the comments below to guide you!)
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
-
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
+   
   }
 
-  // We should kick of the animation of the rock around here
-
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision
+ 
   ROCKS.push(rock)
 
   // Finally, return the rock element you've created
